f186d5cbe958550cb957d63f6f3c0e14b2b1cc6a
oskarcokl <oskar.cokl@gmail.com>
2025-03-15 13:17:15 +0100

    feat: Processing args from the command line

--- src/github-activity.ts
+++ src/github-activity.ts
@@ -1,1 +1,27 @@
 #!/usr/bin/env node
+
+import { exit } from 'process';
+
+const endpoint = (userName: string) => `https://api.github.com/users/${userName}/events/public`;
+
+function main(args: string[]) {
+    const username = args[0];
+
+    if (typeof username === 'undefined') {
+        printHelpMessage();
+        console.error('ERROR: No username provided');
+        exit(1);
+    }
+}
+
+const helpMessage = `
+You can use this script to view a users github activity
+
+Usage:
+    github-activity.js <user-name>
+`;
+function printHelpMessage() {
+    console.log(helpMessage);
+}
+
+main(process.argv.slice(2, process.argv.length));
